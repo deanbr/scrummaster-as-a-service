621e67602475d75fc67161b5eaa8487e00c8d018
Brandon Dean <brandon.dean@wpengine.com>
2019-07-23 13:36:21 -0500

    cleaned up and set up basic styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Quote from  './components/quote';
 import './App.css';
+var FontAwesome = require('react-fontawesome');
 
 class App extends Component {
   state = {
     quotes: [
       'test quote 1',
       'test quote 2'
-    ]
+    ],
+    selectedQuote: null
   }
 
   render() {
     return (
       <div className='App'>
-        <Quote quotes={this.state.quotes} />
+        <h1>Scrum Master as a Service</h1>
+        <Quote quote={this.state.selectedQuote} />
+        <button>Impart some wisdom <FontAwesome name="hat-wizard" /></button>
       </div>
     );
   }
--- src/components/quote.js
+++ src/components/quote.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 
 class Quote extends Component {
   render() {
-    return <div>{this.props.quotes.map((quote, i) =>
-        <div key={i}>
-          <span>{quote}</span>
-          <br />
-        </div>
-    )}</div>;
+    return <div>{this.props.quote}</div>;
   }
 }
 
